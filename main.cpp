3a86409b8ac886b654496ade5c83e320bf01ef90
Bob (AntonenkoVV) <antonenkovera05@gmail.com>
2024-07-07 18:45:19 +0300

    добавлено произведение чисел

--- main.cpp
+++ main.cpp
@@ -9,5 +9,6 @@ int main()
     cin >> a >> b;
 
     cout << "a + b = " << a + b << endl
-         << "a - b = " << a - b << endl;
+         << "a - b = " << a - b << endl
+         << "a * b = " << a * b << endl;
 }
